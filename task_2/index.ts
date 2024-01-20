91c26f4fff243f7fb1c9830cf304d8353588a970
radluki <radluki@gmail.com>
2024-01-20 21:36:21 +0000

    index cleanup

--- task_2/index.ts
+++ task_2/index.ts
@@ -3,7 +3,7 @@ import { updateResource, getResource } from "./src/manageResource";
 import type { APIGatewayEvent } from "aws-lambda";
 import HttpStatus from "http-status-codes";
 
-function validateEvent(event: Partial<APIGatewayEvent>) {
+function validateEvent(event: Partial<APIGatewayEvent>): Event | undefined {
   const { userId, resourceId } = event.pathParameters!;
   const action = event.httpMethod;
   const isValid = (userId && resourceId && action === "PATCH") || action === "GET";
@@ -13,31 +13,47 @@ function validateEvent(event: Partial<APIGatewayEvent>) {
         resourceId: <string>resourceId,
         action: <"PATCH" | "GET">action,
       }
-    : false;
+    : undefined;
+}
+
+type Action = "PATCH" | "GET";
+class Event {
+  constructor(public userId: string, public resourceId: string, public action: Action) {}
+}
+
+async function dispatchEvent(event: Event) {
+  const { userId, resourceId, action } = event;
+  if (action === "GET") {
+    return await getResource(resourceId)
+      .then((value) => ({ statusCode: HttpStatus.OK, body: { value } }))
+      .catch((e) => ({ statusCode: HttpStatus.NOT_FOUND, body: { message: e.message } }));
+  } else if (action === "PATCH") {
+    return await updateResource(resourceId, userId).then((value) => ({
+      statusCode: HttpStatus.ACCEPTED,
+      body: { value },
+    }));
+  }
+  return { statusCode: HttpStatus.BAD_REQUEST, body: { message: "unsupported action" + action } };
 }
 
 export const handler = async function (event: Partial<APIGatewayEvent>) {
   try {
-    const validatedEventParams = validateEvent(event);
-    if (!validatedEventParams)
-      return { statusCode: 400, body: JSON.stringify({ message: "Bad request" }) };
-    const { userId, resourceId, action } = validatedEventParams;
+    const validatedEvent = validateEvent(event);
+    if (!validatedEvent)
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        body: JSON.stringify({ message: "Bad request" }),
+      };
 
-    const isAuthorized = await authorized(userId, resourceId, action);
-    if (!isAuthorized)
-      return { statusCode: 403, body: JSON.stringify({ message: "Unauthorized" }) };
+    if (
+      !(await authorized(validatedEvent.userId, validatedEvent.resourceId, validatedEvent.action))
+    )
+      return {
+        statusCode: HttpStatus.FORBIDDEN,
+        body: JSON.stringify({ message: "Unauthorized" }),
+      };
 
-    let res = { statusCode: HttpStatus.INTERNAL_SERVER_ERROR, body: <any>{} };
-    if (action === "GET") {
-      res = await getResource(resourceId)
-        .then((value) => ({ statusCode: HttpStatus.OK, body: { value } }))
-        .catch((e) => ({ statusCode: HttpStatus.NOT_FOUND, body: { message: e.message } }));
-    } else if (action === "PATCH") {
-      res = await updateResource(resourceId, userId).then((value) => ({
-        statusCode: HttpStatus.ACCEPTED,
-        body: { value },
-      }));
-    }
+    const res = await dispatchEvent(validatedEvent);
     return { statusCode: res.statusCode, body: JSON.stringify(res.body) };
   } catch (error) {
     return { statusCode: HttpStatus.INTERNAL_SERVER_ERROR, body: JSON.stringify({ error }) };
